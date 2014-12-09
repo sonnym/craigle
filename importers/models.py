48e472516c581a5067180bd748268f3ae369519e
sonnym <michaud.sonny@gmail.com>
2014-12-09 18:44:28 -0500

    decorate run methods as jobs

--- importers/models.py
+++ importers/models.py
@@ -1,6 +1,8 @@
 from urllib.parse import urljoin
 from urllib.request import urlopen
 
+from django_rq import job
+
 from parsers.models import SiteParser, CityParser
 from cities.models import City
 from posts.models import Post
@@ -9,12 +11,12 @@ class SiteImporter():
     url = 'https://www.craigslist.org/about/sites'
 
     @classmethod
+    @job
     def run(cls):
         response = urlopen(cls.url)
         html = response.read()
 
         cities = SiteParser().run(html)
-
 
         for city in cities:
             City.create_or_update(city)
@@ -23,9 +25,8 @@ class CityImporter():
     path = 'search/jjj/?cat_id=14&cat_id=21&cat_id=11&is_telecommuting=1&is_contract=1'
 
     @classmethod
+    @job
     def run(cls, City):
-        # import pdb; pdb.set_trace()
-
         response = urlopen(urljoin(City.url, cls.path, allow_fragments=True))
         html = response.read()
 
