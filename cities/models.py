4c7016e1cd33f4e92b7ca9eadc0ea815ba0e4d36
sonnym <michaud.sonny@gmail.com>
2014-12-09 03:00:53 -0500

    basic method to create or update cities

--- cities/models.py
+++ cities/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
 
 class City(models.Model):
     name = models.CharField(max_length=127)
     url = models.CharField(max_length=127)
+
+    @classmethod
+    def create_or_update(cls, city_data):
+        try:
+            city = City.objects.get(name=city_data['name'])
+            city.url = city_data['url']
+
+        except ObjectDoesNotExist:
+            city = City(**city_data)
+
+        city.save()
--- cities/tests.py
+++ cities/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from cities.models import City
 
-# Create your tests here.
+class CityTest(TestCase):
+    def test_create_or_update(self):
+        data = { 'name': 'foo', 'url': 'bar' }
+
+        City.create_or_update(data)
+        self.assertEqual(1, City.objects.count())
+
+        City.create_or_update(data)
+        self.assertEqual(1, City.objects.count())
