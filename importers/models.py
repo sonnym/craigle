7520214546a6d4dae940ee51af213dcba7f32f16
sonnym <michaud.sonny@gmail.com>
2014-12-09 03:25:18 -0500

    basic city importer

--- importers/models.py
+++ importers/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from urllib.request import urlopen
 
-# Create your models here.
+from parsers.models import CityParser
+from cities.models import City
+
+class CityImporter():
+    url = 'https://www.craigslist.org/about/sites'
+
+    @classmethod
+    def run(cls):
+        response = urlopen(cls.url)
+        html = response.read()
+
+        cities = CityParser().run(html)
+
+
+        for city in cities:
+            City.create_or_update(city)
--- importers/tests.py
+++ importers/tests.py
@@ -1,3 +1,18 @@
+from unittest.mock import Mock, patch
 from django.test import TestCase
 
-# Create your tests here.
+from importers.models import CityImporter
+from parsers.models import CityParser
+from cities.models import City
+
+class CityImporterTest(TestCase):
+    def test_run(self):
+        cities = [{ 'name': 'foo', 'url': 'bar' }]
+
+        mock = Mock(City.create_or_update)
+        City.create_or_update = mock
+
+        with patch.object(CityParser, 'run', return_value=cities) as mock_method:
+            CityImporter.run()
+
+            mock.assert_called_once_with(cities[0])
