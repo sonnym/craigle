ed27b8b20daa5f2169b40b911c7033600628a96e
sonnym <michaud.sonny@gmail.com>
2014-12-09 18:30:19 -0500

    class for importing cities

--- importers/models.py
+++ importers/models.py
@@ -1,7 +1,9 @@
+from urllib.parse import urljoin
 from urllib.request import urlopen
 
-from parsers.models import SiteParser
+from parsers.models import SiteParser, CityParser
 from cities.models import City
+from posts.models import Post
 
 class SiteImporter():
     url = 'https://www.craigslist.org/about/sites'
@@ -16,3 +18,18 @@ class SiteImporter():
 
         for city in cities:
             City.create_or_update(city)
+
+class CityImporter():
+    path = 'search/jjj/?cat_id=14&cat_id=21&cat_id=11&is_telecommuting=1&is_contract=1'
+
+    @classmethod
+    def run(cls, City):
+        # import pdb; pdb.set_trace()
+
+        response = urlopen(urljoin(City.url, cls.path, allow_fragments=True))
+        html = response.read()
+
+        posts = CityParser().run(html)
+
+        for post in posts:
+            Post.create_or_update(urljoin(City.url, post))
--- importers/tests.py
+++ importers/tests.py
@@ -1,9 +1,11 @@
 from unittest.mock import Mock, patch
 from django.test import TestCase
 
-from importers.models import SiteImporter
-from parsers.models import SiteParser
+from importers.models import SiteImporter, CityImporter
+from parsers.models import SiteParser, CityParser
+
 from cities.models import City
+from posts.models import Post
 
 class SiteImporterTest(TestCase):
     def test_run(self):
@@ -16,3 +18,16 @@ class SiteImporterTest(TestCase):
             SiteImporter.run()
 
             mock.assert_called_once_with(cities[0])
+
+class CityImporterTest(TestCase):
+    def test_run(self):
+        city = Mock(url='http://nyc.craigslist.org/')
+        posts = ['/foo/bar']
+
+        mock = Mock(Post.create_or_update)
+        Post.create_or_update = mock
+
+        with patch.object(CityParser, 'run', return_value=posts) as mock_method:
+            CityImporter.run(city)
+
+            mock.assert_called_once_with('http://nyc.craigslist.org/foo/bar')
