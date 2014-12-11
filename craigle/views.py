b5f93cd2730be41e7d3295444011bbfe3e1318f1
sonnym <michaud.sonny@gmail.com>
2014-12-11 17:12:35 -0500

    display one hundred most recent posts on home page

--- craigle/views.py
+++ craigle/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+from posts.models import Post
+
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.objects.order_by('-id')[:100]
+
+    return render(request, 'home.html', { 'posts': posts })
--- tests/functional/test_access.py
+++ tests/functional/test_access.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, Client
 
+from posts.models import Post
+
 class AccessTest(TestCase):
     def setup():
         self.client = Client()
@@ -8,3 +10,11 @@ class AccessTest(TestCase):
        response = self.client.get('/')
 
        self.assertEqual(200, response.status_code)
+
+    def test_can_see_posts(self):
+        Post(url='http://example.com/', title='Some Title', compensation='Some Compensation').save()
+
+        response = self.client.get('/')
+
+        self.assertContains(response, 'Some Title', 1)
+        self.assertContains(response, 'Some Compensation', 1)
