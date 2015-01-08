44ab6cf678cdacf6391a63731aabe2aec72017f6
sonnym <michaud.sonny@gmail.com>
2015-01-07 20:19:07 -0500

    fix test broken by excluding empty posted at field

--- tests/functional/test_access.py
+++ tests/functional/test_access.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from django.test import TestCase, Client
+from django.utils.timezone import utc
 
 from posts.models import Post
 
@@ -12,7 +15,7 @@ class AccessTest(TestCase):
        self.assertEqual(200, response.status_code)
 
     def test_can_see_posts(self):
-        Post(url='http://example.com/', title='Some Title', compensation='Some Compensation').save()
+        Post(url='http://example.com/', title='Some Title', compensation='Some Compensation', posted_at=datetime.utcnow().replace(tzinfo=utc)).save()
 
         response = self.client.get('/')
 
