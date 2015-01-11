a86a23b4ed0f929cf079df44ab1536eab78db436
sonnym <michaud.sonny@gmail.com>
2015-01-11 15:31:15 -0500

    add django-smuggler as a dependency

--- craigle/urls.py
+++ craigle/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', 'craigle.views.home', name='home'),
 
+    url(r'^admin/', include('smuggler.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^django-rq/', include('django_rq.urls')),
 )
